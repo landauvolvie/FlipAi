53ed7fcdb41abfbe41ce8b6377f188d02e643660
landauvolvie <landauvolvie@gmail.com>
2026-08-25 09:47:15 -0400

    Set internal version to 0.20.1

--- config.go
+++ config.go
@@ -15,7 +15,7 @@ import (
 	"time"
 )
 
-const version = "0.20.0"
+const version = "0.20.1"
 
 // defaultReplyStyleHint is the only behavioural framing FlipAi adds to an SMS
 // command. FlipAi delivers the reply itself, so the agent is never told how or
