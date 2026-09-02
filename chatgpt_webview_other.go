d6b5edc7c99f7c7d64658470d4d1949d6a8ed94a
landauvolvie <landauvolvie@gmail.com>
2026-09-02 09:22:41 -0400

    Add non-Windows ChatGPT WebView stubs

---
+++ chatgpt_webview_other.go
@@ -0,0 +1,12 @@
+//go:build !windows
+
+package main
+
+import "errors"
+
+var errChatGPTWebViewWindowsOnly = errors.New("the dedicated ChatGPT browser is available on Windows only")
+
+func platformStartChatGPTLogin(string) error { return errChatGPTWebViewWindowsOnly }
+func platformEnsureChatGPTWorker(string) error { return errChatGPTWebViewWindowsOnly }
+func platformStopChatGPTWorker(string) error { return nil }
+func chatGPTWorkerMain(string, bool) {}
