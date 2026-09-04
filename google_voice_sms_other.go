e3a538262ab4b68f0386c797b4d3e07959c3e455
landauvolvie <landauvolvie@gmail.com>
2026-09-04 18:58:59 -0400

    Add non-Windows Google Voice SMS browser stubs

--- google_voice_sms_other.go
+++ google_voice_sms_other.go
@@ -10,3 +10,15 @@ import (
 func requestGoogleVoiceText(context.Context, string, string, string) error {
 	return errors.New("direct Google Voice SMS is available on Windows only")
 }
+
+func platformStartGoogleVoiceSMSLogin(string) error {
+	return errors.New("direct Google Voice SMS is available on Windows only")
+}
+
+func platformEnsureGoogleVoiceSMSWorker(string) error {
+	return errors.New("direct Google Voice SMS is available on Windows only")
+}
+
+func platformStopGoogleVoiceSMSWorker(string) error { return nil }
+
+func platformDisconnectGoogleVoiceSMS(string) error { return nil }
