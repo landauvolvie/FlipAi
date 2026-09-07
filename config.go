b32a42b951e011a8e275774b8e461aa1e8404912
landauvolvie <landauvolvie@gmail.com>
2026-09-06 23:03:45 -0400

    Sync compiled version to v0.46.55

--- config.go
+++ config.go
@@ -15,7 +15,7 @@ import (
 	"time"
 )
 
-const version = "0.46.54"
+const version = "0.46.55"
 
 // defaultReplyStyleHint is the only behavioural framing FlipAi adds to a phone
 // command. It deliberately says nothing about SMS or plain text so providers
